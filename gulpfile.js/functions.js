aadeb6dd23249c8b9bcdc3b4e67425177c290f98
Lars Lehners <lars.lehners@networkteam.com>
2017-10-25 08:32:35 +0200

    WIP: Integrate buildpath() into proxy setting
    * Not testet yet
    * There still has to be the option to set an own proxy

--- gulpfile.js/functions.js
+++ gulpfile.js/functions.js
@@ -250,6 +250,31 @@ function notifyText(object) {
 	}
 }
 
+function buildPath() {
+	const workingDirectory = __dirname,
+		folder = workingDirectory.split('/'),
+		os = require('os');
+
+	// check if we're working local or on typokeeper
+	if (os.hostname().match(/local$/)) {
+		const vhostIndex = folder.indexOf('src') + 1;
+		// construct the base url from parts of our working directory
+		// customer name
+		return 'http://' + folder[vhostIndex] + '.flow.dev';
+	} else {
+		// check if we are in the users directory
+		if (workingDirectory.indexOf('vhosts')) {
+			// construct the base url from parts of our working directory
+			//				   customer name	 username
+			return 'http://' + folder[4] + '.' + folder[2] + '.cms.dev.interner-server.de/';
+		} else {
+			// construct the base url from parts of the working directory
+			//				   customer name is this time on the second position, because we are not in a user dir
+			return 'http://' + folder[2] + '.dev.interner-server.de/';
+		}
+	}
+}
+
 module.exports = {
 	globalImport: globalImport,
 	getTimestamp: getTimestamp,
@@ -257,5 +282,6 @@ module.exports = {
 	mergeConfigAndLoadTasks: mergeConfigAndLoadTasks,
 	getFilesToWatch: getFilesToWatch,
 	pluralize: pluralize,
-	notifyText: notifyText
+	notifyText: notifyText,
+	buildPath: buildPath
 };
